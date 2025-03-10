2a69662c357a2c8b087167f8c97374a30c601bfe
Andre-LSL <andreluizsilvalm@gmail.com>
2025-03-10 16:51:25 -0300

    Resolução

--- src/view/Main.java
+++ src/view/Main.java
@@ -1,3 +1,30 @@
-public class Main {
+package view;
 
+import javax.swing.JOptionPane;
+import controller.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int tamanho = Integer.parseInt(JOptionPane.showInputDialog("Digite o tamanho do vetor: "));
+        int[] vetor = new int[tamanho];
+
+        for (int i = 0; i < tamanho; i++) {
+            vetor[i] = Integer.parseInt(JOptionPane.showInputDialog("Digite um número para a posição " + i + ": "));
+        }
+
+        System.out.print("Vetor antes da ordenação: ");
+        for (int num : vetor) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+
+        Metodos ordenacao = new Metodos();
+        ordenacao.mergeSort(vetor, 0, vetor.length - 1);
+
+        System.out.print("Vetor ordenado: ");
+        for (int num : vetor) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
 }
